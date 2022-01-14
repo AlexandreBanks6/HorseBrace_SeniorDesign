2691518ce4c41602b45e6db9fc8d6d3aac96b7c0
AlexandreBanks6 <65726682+AlexandreBanks6@users.noreply.github.com>
2022-01-14 11:35:10 -0400

    Update to Matlab UI

--- MATLAB_UI/MATLABUI_MainScript.m
+++ MATLAB_UI/MATLABUI_MainScript.m
@@ -8,6 +8,7 @@ recording commands.
 %}
 clear
 clc
+ReHorseUI();
 %% Defining the app
 % fig=uifigure;
 % fig.Name="ReHORSE Tech";
@@ -28,38 +29,113 @@ clc
 
 %% Connecting to the bluetooth module
 %show the bluetooth list (for debugging only)
-list=bluetoothlist;
-%Connect to the HC-05 bluetooth module
+% list=bluetoothlist;
+% %Connect to the HC-05 bluetooth module
 device=bluetooth("HC-05");  
 ReHorseUI(device);
 
-%% UI Functions
+%% UI Function
 function ReHorseUI(device)
-fig=uifigure('Position',[700,350,200,200]);
 
-%Create a pushbutton
+%Creates a figure window
+fig=uifigure('Position',[700,350,250,350]);
+fig.Color='#ADD8E6';   %Royal blue background colour
+
+%~~~~~~<Add 4 pushbuttons to the UI figure>~~~~~~~~~~~
+% Button 1=read data file ('R')
+% Button 2=start recording ('I')
+% Button 3=stop recording ('O')
+% Button 4=calibrate ('C')
+
 b1=uibutton(fig,'push',...
-    'Position',[50,50,50,100],...
-    'ButtonPushedFcn',@(b1,event) WriteGreen(b1,device));    %Calls the function to turn on the green LED through HC-05
-b1.Text='Green';
+    'Position',[25,200,100,100],...
+    'ButtonPushedFcn',@(b1,event) ReadData(b1,device));    %Calls the function to read data through HC-05
+b1.Text='Read Data File';
+b1.FontName='Roboto Medium';
+set(b1,'Backgroundcolor','#e18b6d');
+
+
 b2=uibutton(fig,'push',...
-    'Position',[100,50,50,100],...
-    'ButtonPushedFcn',@(b2,event) WriteRed(b2,device));    %Calls the function to turn on the green LED through HC-05
-b2.Text='Red';
+    'Position',[125,200,100,100],...
+    'ButtonPushedFcn',@(b2,event) StartRecording(b2,device));    %Calls the function to start recording gait data
+b2.Text='Start Recording';
+b2.FontName='Roboto Medium';
+set(b2,'Backgroundcolor','#e18b6d');
+
+b3=uibutton(fig,'push',...
+    'Position',[25,100,100,100],...
+    'ButtonPushedFcn',@(b3,event) StopRecording(b3,device));    %Calls the function to stop recording gait data
+b3.Text='Stop Recording';
+b3.FontName='Roboto Medium';
+set(b3,'Backgroundcolor','#e18b6d');
+
+b4=uibutton(fig,'push',...
+    'Position',[125,100,100,100],...
+    'ButtonPushedFcn',@(b4,event) Calibrate(b4,device));    %Calls the function to start recording gait data
+b4.Text='Calibrate Device';
+b4.FontName='Roboto Medium';
+set(b4,'Backgroundcolor','#e18b6d');
+
+% Adding Title Text to UI
+lblTitle=uilabel(fig);
+lblTitle.Position=[25,325,200,25];
+lblTitle.Text='Make a Selection';
+lblTitle.FontSize=16;
+lblTitle.HorizontalAlignment='center';
+lblTitle.FontName='Roboto Medium';
+
+
+%Adding instructional Text for Calibration
+lblIns=uilabel(fig);
+lblIns.Position=[25,300,200,25]; %Sets the position
+lblIns.Text='Ensure Horse is Standing Still at Calibration';
+lblIns.HorizontalAlignment='center';
+lblIns.FontSize=10;
+lblIns.FontName='Roboto Light';
+
+%Adding Company Name
+lblName=uilabel(fig);
+lblName.Position=[25,25,75,50];
+lblName.Text='ReHorse Technologies';
+lblName.WordWrap='on'; %Makes the text two lines
+lblName.FontSize=12;
+lblName.FontName='Playfair Display';
+
+%Adding Year
+lblYear=uilabel(fig);
+lblYear.Position=[175,25,75,50];
+lblYear.Text='V1.1,     Updated 2022';
+lblYear.WordWrap='on';
+lblYear.FontSize=12;
+lblYear.FontName='Roboto Light';
+
+
 
 end
 
-function WriteGreen(b1,device)
-ReadDataCommand='G';  %Sends 'G' char to turn on the green LED
-%device=bluetooth("HC-05"); 
-write(device,ReadDataCommand);  %Writes the green command to the HC-05 module
+
+%% Bluetooth Interface Functions
+function ReadData(b1,device)
+Command='G';  %Sends 'R' char to read the data file
+write(device,Command);  %Writes the read command to the HC-05 module
 
 end
 
-function WriteRed(b2,device)
-ReadDataCommand='R';  %Sends 'R' char to turn on the red LED
-%device=bluetooth("HC-05"); 
-write(device,ReadDataCommand);  %Writes the red command to the HC-05 module
+function StartRecording(b2,device)
+Command='I';  %Sends 'I' char to start recording data 
+write(device,Command);  %Writes the start recording command to the HC-05 module
+
+end
+
+function StopRecording(b3,device)
+Command='O';  %Sends 'O' char to start recording data 
+write(device,Command);  %Writes the stop recording command to the HC-05 module
+
+end
+
+function Calibrate(b4,device)
+Command='C';  %Sends 'C' char to calibrate the sensors 
+write(device,Command);  %Writes the calibrate command to the HC-05 module
 
 end
 
